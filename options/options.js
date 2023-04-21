9d8a9cf4ca3e1bbc28c5577eb5abe89ef0801048
Crowdbotics <team@crowdbotics.com>
2023-04-21 04:23:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mud-mash-40075",
+  url: "https://mud-mash-40075.botics.co",
+  api: "https://mud-mash-40075.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
